6d76cd0d63dfbed70716ef6e50b27b937cbc40d5
Joshua Sarmiento <supremojoshua22@gmail.com>
2023-11-24 22:57:11 +0800

    update

--- index.js
+++ index.js
@@ -4,41 +4,20 @@ const bodyParser = require('body-parser')
 const crypto = require('crypto')
 const cors = require('cors')
 const KJUR = require('jsrsasign')
-// const { createProxyMiddleware } =   require('http-proxy-middleware');
 
 const app = express()
 const port = process.env.PORT || 4000
 
-app.use(bodyParser.json())
-app.options('*', cors({
-  origin: '*', // You can restrict the origin here
-  methods: ['GET', 'POST', 'DELETE', 'PUT'], // Allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed Headers
-}))
+app.use(bodyParser.json(), cors())
+app.options('*', cors())
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
+app.post('/', (req, res) => {
 
-const handler = (req, res) => {
   const iat = Math.round(new Date().getTime() / 1000) - 30;
   const exp = iat + 60 * 60 * 2
- 
+
   const oHeader = { alg: 'HS256', typ: 'JWT' }
- 
+
   const oPayload = {
     sdkKey: process.env.ZOOM_MEETING_SDK_KEY,
     mn: req.body.meetingNumber,
@@ -48,20 +27,14 @@ const handler = (req, res) => {
     appKey: process.env.ZOOM_MEETING_SDK_KEY,
     tokenExp: iat + 60 * 60 * 2
   }
- 
+
   const sHeader = JSON.stringify(oHeader)
   const sPayload = JSON.stringify(oPayload)
   const signature = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, process.env.ZOOM_MEETING_SDK_SECRET)
-  
-  res.header('Access-Control-Allow-Origin', 'https://zoom-vue.vercel.app');  // Add this line
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');  // Add this line
-  res.header('Access-Control-Allow-Headers', 'Content-Type');  // Add this line
 
   res.json({
     signature: signature
-  });
- }
- 
- app.post('/', allowCors(handler))
+  })
+})
 
 app.listen(port, () => console.log(`Zoom Meeting SDK Auth Endpoint Sample Node.js listening on port ${port}!`))
