6f0b52947cedf1139a4a5acb99bd7c8e55752557
Joshua Sarmiento <supremojoshua22@gmail.com>
2023-11-24 17:45:20 +0800

    update index

--- index.js
+++ index.js
@@ -4,37 +4,51 @@ const bodyParser = require('body-parser')
 const crypto = require('crypto')
 const cors = require('cors')
 const KJUR = require('jsrsasign')
-
+  
 const app = express()
 const port = process.env.PORT || 4000
 
-app.use(bodyParser.json(), cors())
+app.use(bodyParser.json())
+app.use(cors())
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://zoom-vue.onevillar.ph");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.options('*', cors())
 
 app.post('/', (req, res) => {
+  try {
+    const iat = Math.round(new Date().getTime() / 1000) - 30;
+    const exp = iat + 60 * 60 * 2
 
-  const iat = Math.round(new Date().getTime() / 1000) - 30;
-  const exp = iat + 60 * 60 * 2
+    const oHeader = { alg: 'HS256', typ: 'JWT' }
 
-  const oHeader = { alg: 'HS256', typ: 'JWT' }
+    const oPayload = {
+      sdkKey: process.env.ZOOM_MEETING_SDK_KEY,
+      mn: req.body.meetingNumber,
+      role: req.body.role,
+      iat: iat,
+      exp: exp,
+      appKey: process.env.ZOOM_MEETING_SDK_KEY,
+      tokenExp: iat + 60 * 60 * 2
+    }
 
-  const oPayload = {
-    sdkKey: process.env.ZOOM_MEETING_SDK_KEY,
-    mn: req.body.meetingNumber,
-    role: req.body.role,
-    iat: iat,
-    exp: exp,
-    appKey: process.env.ZOOM_MEETING_SDK_KEY,
-    tokenExp: iat + 60 * 60 * 2
+    const sHeader = JSON.stringify(oHeader)
+    const sPayload = JSON.stringify(oPayload)
+    const signature = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, process.env.ZOOM_MEETING_SDK_SECRET)
+
+    // res.header('Access-Control-Allow-Origin', 'https://zoom-vue.onevillar.ph');
+    // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    res.json({
+      signature: signature
+    });
+
+  } catch(error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-
-  const sHeader = JSON.stringify(oHeader)
-  const sPayload = JSON.stringify(oPayload)
-  const signature = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, process.env.ZOOM_MEETING_SDK_SECRET)
-
-  res.json({
-    signature: signature
-  })
 })
 
 app.listen(port, () => console.log(`Zoom Meeting SDK Auth Endpoint Sample Node.js listening on port ${port}!`))
